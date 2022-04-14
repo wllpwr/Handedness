e7792f8d2cae1dab90aaeab5fcd56d53074e7178
Will Gesler <willgesler@gmail.com>
2022-04-14 15:05:44 -0400

    TESTS

---
+++ app/src/main/java/com/wllpwr/handedness/BottomNavTest.kt
@@ -0,0 +1,221 @@
+package com.wllpwr.handedness
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.wllpwr.handedness.ui.theme.HandednessTheme
+import androidx.compose.material.Icon as Icon1
+import androidx.compose.ui.graphics.vector.ImageVector
+
+class BottomNavTest : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HandednessTheme {
+                val navController = rememberNavController()
+
+                Surface(color = Color.White) {
+                    // Scaffold Component
+                    Scaffold(
+                        // Bottom navigation
+                        bottomBar = {
+                            BottomNavigationBar(navController = navController)
+                        }, content = { padding ->
+                            // Navhost: where screens are placed
+                            NavHostContainer(navController = navController, padding = padding)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun HomeScreen() {
+
+    // Column Composable,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        // Parameters set to place the items in center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Icon Composable
+        Icon1(
+            imageVector = Icons.Default.Home,
+            contentDescription = "home",
+            tint = Color(0xFF0F9D58)
+        )
+        // Text to Display the current Screen
+        Text(text = "Home", color = Color.Black)
+    }
+}
+
+@Composable
+fun SearchScreen() {
+    // Column Composable,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        // parameters set to place the items in center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Icon Composable
+        Icon1(
+            imageVector = Icons.Default.Search,
+            contentDescription = "search",
+            tint = Color(0xFF0F9D58)
+        )
+        // Text to Display the current Screen
+        Text(text = "Search", color = Color.Black)
+    }
+}
+
+@Composable
+fun ProfileScreen() {
+    // Column Composable,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        // parameters set to place the items in center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Icon Composable
+        Icon1(
+            imageVector = Icons.Default.Person,
+            contentDescription = "Profile",
+            tint = Color(0xFF0F9D58)
+        )
+        // Text to Display the current Screen
+        Text(text = "Profile", color = Color.Black)
+    }
+}
+
+
+data class BottomNavItem(
+    val label: String,
+    val icon: ImageVector,
+    val route:String,
+)
+object Constants {
+    val BottomNavItems = listOf(
+        BottomNavItem(
+            label = "Home",
+            icon = Icons.Filled.Home,
+            route = "home"
+        ),
+        BottomNavItem(
+            label = "Search",
+            icon = Icons.Filled.Search,
+            route = "search"
+        ),
+        BottomNavItem(
+            label = "Profile",
+            icon = Icons.Filled.Person,
+            route = "profile"
+        )
+    )
+}
+@Composable
+fun NavHostContainer(
+    navController: NavHostController,
+    padding: PaddingValues
+) {
+
+    NavHost(
+        navController = navController,
+
+        // set the start destination as home
+        startDestination = "home",
+
+        // Set the padding provided by scaffold
+        modifier = Modifier.padding(paddingValues = padding),
+
+        builder = {
+
+            // route : Home
+            composable("home") {
+                HomeScreen()
+            }
+
+            // route : search
+            composable("search") {
+                SearchScreen()
+            }
+
+            // route : profile
+            composable("profile") {
+                ProfileScreen()
+            }
+        })
+    
+
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+
+    BottomNavigation(
+
+        // set background color
+        backgroundColor = Color(255, 255, 255)) {
+
+        // observe the backstack
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+        // observe current route to change the icon
+        // color,label color when navigated
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        // Bottom nav items we declared
+        Constants.BottomNavItems.forEach { navItem ->
+
+            // Place the bottom nav items
+            BottomNavigationItem(
+
+                // it currentRoute is equal then its selected route
+                selected = currentRoute == navItem.route,
+
+                // navigate on click
+                onClick = {
+                    navController.navigate(navItem.route)
+                },
+
+                // Icon of navItem
+                icon = {
+                    Icon1(imageVector = navItem.icon, contentDescription = navItem.label)
+                },
+
+                // label
+                label = {
+                    Text(text = navItem.label)
+                },
+                alwaysShowLabel = false
+            )
+        }
+    }
+}
+
---
+++ app/src/main/java/com/wllpwr/handedness/BurgerTest.kt
@@ -0,0 +1,146 @@
+package com.wllpwr.handedness
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.wllpwr.handedness.ui.theme.Purple40
+import com.wllpwr.handedness.ui.theme.Purple80
+import com.wllpwr.handedness.ui.theme.PurpleGrey40
+import kotlinx.coroutines.launch
+
+class BurgerTest : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Scaffold(
+                topBar = { TopAppBar(title = { Text("Handedness Test", color = Color.Black, fontWeight = FontWeight.Bold) }, backgroundColor = Purple80) },
+                content = {
+                    Surface(color = Color.White) {
+                        // Scaffold we created
+                        ScaffoldExample()
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun ScaffoldExample() {
+
+    // create a scaffold state, set it to close by default
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+
+    // Create a coroutine scope. Opening of Drawer
+    // and snackbar should happen in background
+    // thread without blocking main thread
+    val coroutineScope = rememberCoroutineScope()
+
+    // Scaffold Composable
+    Scaffold(
+        drawerElevation = 5.dp,
+
+        // pass the scaffold state
+        scaffoldState = scaffoldState,
+
+        // pass the topbar we created
+        topBar = {
+            TopBar(
+                // When menu is clicked open the
+                // drawer in coroutine scope
+                onMenuClicked = {
+                    coroutineScope.launch {
+                        // to close use -> scaffoldState.drawerState.close()
+                        scaffoldState.drawerState.open()
+                    }
+                })
+        },
+
+        // pass the bottomBar we created
+
+        // Pass the body in
+        // content parameter
+        content = {
+            Body()
+        },
+
+        // pass the drawer
+        drawerContent = {
+            Drawer()
+        }
+
+    )
+}
+
+// A function which will receive a
+// callback to trigger to opening the drawer
+@Composable
+fun TopBar(onMenuClicked: () -> Unit) {
+    // TopAppBar Composable
+    TopAppBar(
+        // Provide Title
+        title = {
+            Text(text = "Scaffold Test", color = Color.White)
+        },
+        // Provide the navigation Icon ( Icon on the left to toggle drawer)
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Menu",
+
+                // When clicked trigger onClick
+                // Callback to trigger drawer open
+                modifier = Modifier.clickable(onClick = onMenuClicked),
+                tint = Color.White
+            )
+        },
+        // background color of topAppBar
+        backgroundColor = Purple40
+    )
+}
+
+@Composable
+fun Body() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(text = "MAIN CONTENT", color = PurpleGrey40)
+    }
+}
+
+
+@Composable
+fun Drawer() {
+    // Column Composable
+    Column(
+        Modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
+        // Repeat is a loop which
+        // takes count as argument
+        repeat(5) { item ->
+            Text(text = "Item number $item", modifier = Modifier.padding(8.dp), color = Color.Black)
+        }
+    }
+}
