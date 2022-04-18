99ae735bf055dbe75175a8c0a4001a27a8336932
Alex Waters <AlexanderWatersAWC@gmail.com>
2022-04-18 17:11:26 -0400

    added hand label

--- app/src/main/java/com/wllpwr/handedness/Directions.kt
+++ app/src/main/java/com/wllpwr/handedness/Directions.kt
@@ -116,8 +116,10 @@ fun ShowDirection(direction: String?) {
                     .padding(10.dp),
                 colors = ButtonDefaults.buttonColors(Purple40),
                 onClick = {
-                    mContext.startActivity(chosenTest)
+                    DataObj.addData("hand")
                     DataObj.addData(hand)
+                    mContext.startActivity(chosenTest)
+
                 }) {
                 androidx.compose.material3.Text(
                     text = "Proceed",
